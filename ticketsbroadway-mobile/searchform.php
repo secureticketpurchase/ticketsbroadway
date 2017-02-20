a887ff211d63ee32c12f3b6cf3dac00b7191242a
JohnParks <john.parks@brafton.com>
2017-02-20 13:52:24 -0500

    Updated mobile to leverage event listing page for search results

--- ticketsbroadway-mobile/searchform.php
+++ ticketsbroadway-mobile/searchform.php
@@ -1,6 +1,6 @@
-<form role="search" method="get" id="mobile-searchform" class="mobile-searchform" action="<?php echo home_url( '/' ); ?>">
+<form role="search" method="get" id="mobile-searchform" class="mobile-searchform" action="<?php echo home_url( '/' ); ?>event-results/">
     <div>
-        <input type="search" id="s" name="s" value="" placeholder="Event, date, location or venue" />
+        <input type="search" id="tosearch" name="tosearch" value="" placeholder="Event, date, location or venue" />
 
         <button type="submit" id="searchsubmit" >Search</button>
     </div>
