b59a9ca9187d2df969afef97e23bfc42e7207fd3
JohnParks <john.parks@brafton.com>
2017-03-15 15:26:10 -0400

    added CTA Sidebar widget area to genre archive

--- ticketsbroadway/taxonomy-genre.php
+++ ticketsbroadway/taxonomy-genre.php
@@ -13,38 +13,42 @@
 						<!--<main id="main" class="m-all t-2of3 d-5of7 cf" role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">-->
 
 						<div class="sidebar d-2of7 t-1of3">
+							<div class="widget-area">
+								<?php if ( dynamic_sidebar( 'cta-sidebar' ) ) : ?>
+								<?php endif; ?>
 							</div>
+						</div>
 
-							<div id="post-<?php the_ID(); ?>" class="body-content d-5of7 t-2of3 show-content" <?php post_class('cf'); ?> role="article">
+						<div id="post-<?php the_ID(); ?>" class="body-content d-5of7 t-2of3 show-content" <?php post_class('cf'); ?> role="article">
 
-							<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+						<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-								<article id="post-<?php the_ID(); ?>" <?php post_class('cf'); ?> role="article">
-								
-									<?php get_template_part( "search", "show" ); ?>
+							<article id="post-<?php the_ID(); ?>" <?php post_class('cf'); ?> role="article">
+							
+								<?php get_template_part( "search", "show" ); ?>
+							</article>
+
+						<?php endwhile; ?>
+
+							<?php bones_page_navi(); ?>
+
+						<?php else : ?>
+
+								<article id="post-not-found" class="hentry cf">
+									<header class="article-header">
+										<h1><?php _e( 'Oops, Post Not Found!', 'bonestheme' ); ?></h1>
+									</header>
+									<section class="entry-content">
+										<p><?php _e( 'Uh Oh. Something is missing. Try double checking things.', 'bonestheme' ); ?></p>
+									</section>
+									<footer class="article-footer">
+										<p><?php _e( 'This is the error message in the single-custom_type.php template.', 'bonestheme' ); ?></p>
+									</footer>
 								</article>
 
-							<?php endwhile; ?>
+						<?php endif; ?>
 
-								<?php bones_page_navi(); ?>
-
-							<?php else : ?>
-
-									<article id="post-not-found" class="hentry cf">
-										<header class="article-header">
-											<h1><?php _e( 'Oops, Post Not Found!', 'bonestheme' ); ?></h1>
-										</header>
-										<section class="entry-content">
-											<p><?php _e( 'Uh Oh. Something is missing. Try double checking things.', 'bonestheme' ); ?></p>
-										</section>
-										<footer class="article-footer">
-											<p><?php _e( 'This is the error message in the single-custom_type.php template.', 'bonestheme' ); ?></p>
-										</footer>
-									</article>
-
-							<?php endif; ?>
-
-						</div><!--</main>-->
+					</div><!--</main>-->
 
 				</div>
 
