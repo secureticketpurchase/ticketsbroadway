2a74fed80f3c695b1d4185138d4119517d297f38
JohnParks <john.parks@brafton.com>
2017-03-15 18:00:18 -0400

    Had missed one edit in functions.php

--- ticketsbroadway/functions.php
+++ ticketsbroadway/functions.php
@@ -812,7 +812,7 @@ function display_shows ( $postID, $numPosts = 4, $topSeller = false, $offset = 0
   }
   // check if city option is selected.  If so, use its "shows" post meta array to add that limit to the query
   if ( MICRO_SHOWS != "" ) {
-    $args['post__in'] = return_shows();
+    $args['post__in'] = theme_arr("shows");
   }
 
   $shows = get_posts( $args );
